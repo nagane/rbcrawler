d05ce8c0b27f0d09bed4d253343d15d973eec336
nagane <naganeys@gmail.com>
2017-04-11 16:23:50 +0900

    add gitignore

--- webrick-template.rb
+++ webrick-template.rb
@@ -7,9 +7,9 @@ class TestContentServlet <
 
   def do_GET(req,res)
     res.body = "this is #{req.path}<br>"
-    res.content_type = WEBrick::HTTPUtils.mine_type(
+    res.content_type = WEBrick::HTTPUtils.mime_type(
       req.path_info,
-      WEBrick::HTTPUtils::DefaultMineTypes
+      WEBrick::HTTPUtils::DefaultMimeTypes
     )
   end
 end
@@ -17,4 +17,6 @@ end
 srv = WEBrick::HTTPServer.new(
   :BindAddress => '127.0.0.1', :Port => 7777)
 
-srv.mount
+srv.mount('/', TestContentServlet)
+trap("INT"){ srv.shutdown }
+srv.start

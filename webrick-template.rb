75e60e3b3a73704b330329dc6d0f7f5a8c0c05a3
nagane <naganeys@gmail.com>
2017-04-14 17:58:51 +0900

    change display pages based on the extensions

--- webrick-template.rb
+++ webrick-template.rb
@@ -6,12 +6,35 @@ class TestContentServlet <
   WEBrick::HTTPServlet::AbstractServlet
 
   def do_GET(req,res)
-    res.body = "this is #{req.path}<br>"
+    res.body = case req.path
+               when /\.html$/ ; html_content req.path
+               when /\.txt$/ ; txt_content req.path
+               else;
+               end
+
     res.content_type = WEBrick::HTTPUtils.mime_type(
       req.path_info,
       WEBrick::HTTPUtils::DefaultMimeTypes
     )
   end
+
+  def html_content(path)
+    node = path[0..-6]
+    <<HTML 
+<html><head><title>#{path}</title></head>
+<body><p>
+<a href= "#{node}/1.html">#{node}/1.html</a><br>
+<a href= "#{node}/2.html">#{node}/2.html</a><br>
+<a href= "#{node}.txt">#{node}.txt</a><br>
+<a href= "http://localhost:7777#{node}.org">
+HTML
+
+  end
+
+  def txt_content(path)
+    "Tihs is #{path}"
+  end
+
 end
 
 srv = WEBrick::HTTPServer.new(

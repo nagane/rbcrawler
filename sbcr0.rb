959b9b658995ed4db15361ed4f145a5deac00290
nagane <naganeys@gmail.com>
2017-04-25 19:22:46 +0900

    [wip]

---
+++ sbcr0.rb
@@ -0,0 +1,11 @@
+
+page_source = open("samplepage.html", &:read)
+dates = page_source.scan(%r!(\d+)年 ?(\d+)月 ?(\d+)日<br />!)
+p dates[0,4]
+
+url_titles = page_source.scan(
+  %r!^<a href="(.+?)">(.+?)</a><br />!)
+p url_titles[0,4]
+
+p dates.length
+p url_titles.length
